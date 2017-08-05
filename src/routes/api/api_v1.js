ebdbd6903ada019105e3500f2e6b02deac1ae4e3
Rylius <ryliusk@gmail.com>
2017-08-05 02:05:50 +0200

    Changed API to return simulation state

--- src/routes/api/api_v1.js
+++ src/routes/api/api_v1.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const simulation = require('../../simulation');
+
 router.get('/simulation', (req, res) => {
     // TODO
-    res.send({});
+    res.contentType('application/json');
+    res.send(simulation.state);
 });
 
 module.exports = router;
